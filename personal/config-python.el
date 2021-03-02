166e7f9d9c7a517848bf64e5ad559355cb9c2a1c
Vikas Mishra <vikas.mishra@gmail.com>
2021-03-02 11:25:08 +0530

    Updtated to use ipython for the Python REPL.

--- personal/config-python.el
+++ personal/config-python.el
@@ -23,3 +23,9 @@
   :custom (blacken-line-length 79))
 
 
+;; Use IPython for REPL
+(setq python-shell-interpreter "jupyter"
+      python-shell-interpreter-args "console --simple-prompt"
+      python-shell-prompt-detect-failure-warning nil)
+(add-to-list 'python-shell-completion-native-disabled-interpreters
+             "jupyter")
